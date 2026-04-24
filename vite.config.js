3127567121f0745a9df299ae876022869b040b21
zhoy84 <76227740@qq.com>
2026-04-24 13:06:50 +0800

    优化 Vite 配置以支持 Tailwind v4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    transformer: 'postcss',
+    preprocessorOptions: {
+      css: {
+        '@import': (tailwindcss)
+      }
+    }
+  }
 })
