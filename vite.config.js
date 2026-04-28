c1cfb6a580e839a8af6c3f5b13878c9a3f28c8ea
zhoy84 <76227740@qq.com>
2026-04-28 21:31:20 +0800

    chore: add project title comment to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// LinkSee - 灵犀在线工具箱
 export default defineConfig({
   plugins: [
     react(),
